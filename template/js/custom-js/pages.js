5d3c0a7edf994d41157077f16bdd56b4228dd07c
Matheus <matheus@e-com.club>
2020-11-09 14:38:29 -0300

    chore: link to cart after click buy

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,1 +1,9 @@
 // Add your custom JavaScript for storefront pages here.
+window.storefront.on('widget:@ecomplus/widget-minicart', function () {
+  setTimeout(function () {
+    const buy = document.querySelector('.product__buy button')
+    buy.addEventListener('click', function () {
+      window.location.href = '/app/#/cart'
+    })
+  }, 800)
+})
