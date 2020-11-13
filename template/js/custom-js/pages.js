2bfea31f4f1421ce90ffdcce1bc80a23de695a50
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2020-11-13 17:54:21 -0300

    Update pages.js

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,9 +1,9 @@
 // Add your custom JavaScript for storefront pages here.
-window.storefront.on('widget:@ecomplus/widget-minicart', function () {
-  setTimeout(function () {
-    const buy = document.querySelector('.product__buy button')
-    buy.addEventListener('click', function () {
-      window.location.href = '/app/#/cart'
-    })
-  }, 800)
-})
+// window.storefront.on('widget:@ecomplus/widget-minicart', function () {
+//  setTimeout(function () {
+//    const buy = document.querySelector('.product__buy button')
+//    buy.addEventListener('click', function () {
+//      window.location.href = '/app/#/cart'
+//    })
+//  }, 800)
+// })
