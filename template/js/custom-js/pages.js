f8ff9c75493737468d03a568e5f02dba9d3fe4db
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2020-11-26 00:22:08 -0300

    Update pages.js

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,7 +1,3 @@
 // Add your custom JavaScript for storefront pages here.
- window.storefront.on('widget:@ecomplus/widget-minicart', function () {
-  setTimeout(function () {
-    $ecomConfig.set('default_img_size', 'zoom')
-  }, 800)
- })
+window.$ecomConfig.set('default_img_size', 'zoom')
 
